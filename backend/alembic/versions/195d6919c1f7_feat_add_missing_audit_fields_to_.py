73098357073c542c1a33ba852dd5b31b294886db
MYNKCHOMIYA <mayankchomia@gmail.com>
2026-07-08 17:03:31 +0530

    style: format alembic migration script with black

--- backend/alembic/versions/195d6919c1f7_feat_add_missing_audit_fields_to_.py
+++ backend/alembic/versions/195d6919c1f7_feat_add_missing_audit_fields_to_.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = "195d6919c1f7"
 down_revision: Union[str, Sequence[str], None] = "6f7a1a59cb66"
